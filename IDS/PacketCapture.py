d7b78eb7bebd033364317e084129bdb484e8946d
Bryce Clemenson <ironbryce8@gmail.com>
2025-08-04 19:59:05 -0400

    Fix constructor method name in PacketCapture and improve formatting; add TrafficAnalyzer class for packet analysis

--- IDS/PacketCapture.py
+++ IDS/PacketCapture.py
@@ -1,10 +1,9 @@
 from scapy.all import sniff, IP, TCP
-from collections import defaultdict
 import threading
 import queue
 
 class PacketCapture:
-    def _init_(self):
+    def __init__(self):
         self.packet_queue = queue.Queue()
         self.stop_capture = threading.Event()
 
@@ -16,12 +15,12 @@ class PacketCapture:
     
     def start_capture(self, interface="eth0"):
         def capture_thread():
-            sniff(iface = interface, prn = self.packet_callback,store = 0,stop_filter = lambda _: self.stop_capture.is_set())
-            self.capture_thread = threading.Thread(target = capture_thread)
-            self.capture_thread.start()
+            sniff(iface=interface, prn=self.packet_callback, store=0, stop_filter=lambda _: self.stop_capture.is_set())
+        self.capture_thread = threading.Thread(target=capture_thread)
+        self.capture_thread.start()
        
     def stop(self):
         self.stop_capture.set()
         self.capture_thread.join()
-        
-    
+
+
---
+++ IDS/TrafficAnalyzer.py
@@ -0,0 +1,48 @@
+from scapy.all import IP, TCP
+from collections import defaultdict
+
+class TrafficAnalyzer:
+    def __init__(self):
+        self.connections = defaultdict(list)
+        self.flow_stats = defaultdict(lambda:{
+            'packet_count' : 0,
+            'byte_count': 0,
+            'start_time': None,
+            'last_time': None
+        })
+    def analyze_packet(self,packet):
+        if IP in packet and TCP in packet :
+            ip_src = packet[IP].src
+            ip_dst = packet[IP].dst
+            port_src = packet[TCP].sport
+            port_dst = packet[TCP].dsport
+            
+            flow_key = (ip_src, ip_dst, port_src, port_dst)
+
+            stats =  self.flow_stats[flow_key]
+            stats['packet_count'] += 1
+            stats['byte_count'] += len(packet)
+            current_time = packet.time
+            if not stats['start_time']:
+                stats['start_time'] = current_time
+            stats['last_time'] = current_time
+
+            return self.extract_features(packet,stats)
+        
+    def extract_features(self, packet, stats):
+        duration = stats['last_time'] - stats['start_time']
+        if duration == 0:
+            packet_rate = 0
+            byte_rate = 0
+        else:
+            packet_rate = stats['packet_count'] / duration
+            byte_rate = stats['byte_count'] / duration
+        return {
+            'packet_size': len(packet),
+            'flow_duration': duration,
+            'packet_rate': packet_rate,
+            'byte_rate': byte_rate,
+            'tcp_flags': packet[TCP].flags,
+            'window_size': packet[TCP].window
+        }
+
